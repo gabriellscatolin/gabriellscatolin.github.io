68ed3bcec0859ad98b7d33a406b614ae94b5fc28
anaaliciamsrn-rgb <anaaliciamsrn@gmail.com>
2026-02-09 22:31:09 -0300

    Primeiro projeto

---
+++ main.js
@@ -0,0 +1,24 @@
+import SceneInicial from "./src/Scenes/SceneInicial.js";
+
+//Configuração do jogo phaser
+const config = {
+  type: Phaser.AUTO,  //Phaser escolhe entreWebGL ou Canvas para renderizar
+  pixelArt: true,    //Garantir bordas nítidas para os gráficos pixelados
+  width: 1920,  // Define a eesolução base: 1920x1080
+  height: 1080,  //Resolução full HD
+  backgroundColor: "#000000",  //Define o fundo como preto
+  scale: {
+    mode: Phaser.Scale.FIT, //Ajusta para caber na tela
+    autoCenter: Phaser.Scale.CENTER  //Centraliza o jogo
+  },
+  physics: {
+    default: "arcade",  //Usa o sistema de física arcade (Jogos 2D)
+    arcade: {
+      gravity: { y: 0 }, //jogo sem quedas então desativa a gravidade vertical
+      debug: false   //Desativa linhas de depuração
+    }
+  },
+  scene: [SceneInicial]
+};
+
+new Phaser.Game(config); //Cria o jogo
---
+++ src/scenes/SceneInicial.js
@@ -0,0 +1,100 @@
+// Cena inicial do jogo com menu principal
+export default class SceneInicial extends Phaser.Scene {
+  constructor() {
+    super("SceneInicial");
+
+// Configurações da cena (Partes fixas)
+    this.CONFIG = {
+      ASSETS: {
+        botaoJogar: "src/assets/imagens/imagensBotoes/botaoJogar.png",   //Botão "jogar 
+        fundo: "src/assets/imagens/imagensMapa/mapaInicial.png",         //Fundo de tela inicial
+        botaoConfig: "src/assets/imagens/imagensBotoes/botaoc.png",      //Botão "configurações"
+        botaoSair: "src/assets/imagens/imagensBotoes/botaosair.png",     //Botão "sair"
+        configFundo: "src/assets/imagens/imagensPopUps/fundoConfig.png"  //Tela de fundo das configurações
+      },
+//Definição de posição e tamanho dos botões
+     BOTOES: [
+    { key: "botaoConfig", x: "center", y: 870, scale: 0.5, action: "openSettings" },
+    { key: "botaoJogar", x: "center", y: 600, scale: 0.5, action: "startGame" },
+    { key: "botaoSair", x: "center", y: 750, scale: 0.5, action: "fecharJogo" }
+]
+    };
+  }
+  
+
+// Carrega os assets
+  preload() {
+    this.load.image("fundo", this.CONFIG.ASSETS.fundo);
+    this.load.image("botaoJogar", this.CONFIG.ASSETS.botaoJogar);
+    this.load.image("botaoConfig", this.CONFIG.ASSETS.botaoConfig);
+    this.load.image("botaoSair", this.CONFIG.ASSETS.botaoSair);
+  }
+//Configura os elementos visuais e interativos das cenas
+  create() {
+    // Fundo
+    this.fundo = this.add.image(0, 0, "fundo").setOrigin(0, 0);
+    this.redimensionarFundo();
+
+    // Botões
+    this.adicionarBotoes();
+
+    this.input.keyboard.on("keydown-F", () => {
+  if (this.scale.isFullscreen) {
+    this.scale.stopFullscreen();  //Ao apertar "F" entra em tela cheia
+  } else {
+    this.scale.startFullscreen();
+  }
+});
+
+// Resize
+    window.addEventListener("resize", this.redimensionarFundo.bind(this));
+  } //Redimensionar fundo de tela
+
+  
+
+  adicionarBotoes() {
+    this.CONFIG.BOTOES.forEach(botao => {
+      let x = botao.x === "center" ? this.scale.width / 2 : botao.x;//Centraliza x se "Center"
+      let y = botao.y;
+
+      const btn = this.add.image(x, y, botao.key)
+        .setScale(botao.scale)
+        .setInteractive({ useHandCursor: true });
+
+    //Define ações ao clicar nos botões
+      btn.on("pointerdown", () => {
+        this[botao.action](); //Executa a ação associada
+      });
+
+      // efeito visual quando passa o mouse
+      btn.on("pointerover", () => btn.setScale(botao.scale * 1.07)); //Botão aumenta em x1.05 ao aproximar o mouse
+      btn.on("pointerout", () => btn.setScale(botao.scale));  //Ao tirar o mouse botão retorna ao tamanho normal
+    });
+  }
+
+  redimensionarFundo() {
+    const largura = this.scale.width;  //Redimensionar largura
+    const altura = this.scale.height;  // Redimensionar altura
+
+    this.fundo.displayWidth = largura;
+    this.fundo.displayHeight = altura;
+  }
+
+  
+
+  startGame() {
+    console.log("Iniciar jogo");
+    // this.scene.start("SceneJogo");
+  }
+
+  openSettings() {
+    console.log("Abrir configurações");
+  }
+
+  fecharJogo() {
+    console.log("Fechar jogo");
+    // window.close(); // navegador bloqueia normalmente
+  }
+}
+
+
