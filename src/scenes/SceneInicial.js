8884104dfb1be0438b9a2da45b6c073c3fc8a37a
anaaliciamsrn-rgb <anaaliciamsrn@gmail.com>
2026-02-11 14:55:07 -0300

    new

--- src/scenes/SceneInicial.js
+++ src/scenes/SceneInicial.js
@@ -93,7 +93,7 @@ export default class SceneInicial extends Phaser.Scene {
     duration: this.CONFIG.PIXELATE_DURATION, //Duração do pixelado
     amount: 1,
     onComplete: () => {
-      this.scene.start("SceneJogo");
+      this.scene.start("ScenePersonagem"); //Inicia a cena da escolha do personagem após o efeito de pixelado
     }
   });
 }
